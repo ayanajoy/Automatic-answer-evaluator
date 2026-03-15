635cc2a41555d445aa57db0b43830a89cd16e2dd
Ayana Joy <169181983+ayanajoy@users.noreply.github.com>
2026-03-15 13:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+import os
+import uvicorn
+
 from routes.teacher_routes import router as teacher_router
 from routes.student_routes import router as student_router
 from routes.auth_routes import router as auth_router
@@ -15,12 +18,15 @@ app.include_router(student_router)
 
 # ================= STATIC FILES =================
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# uploaded question papers / answers
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
 
 # ================= HOME PAGE =================
 @app.get("/")
 def home():
     return FileResponse("static/index.html")
+
+
+# ================= START SERVER =================
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
